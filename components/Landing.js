bac57fda6e2f422add4cce82e0019a22612e4e35
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-16 23:04:28 +0700

    landing

--- components/Landing.js
+++ components/Landing.js
@@ -4,7 +4,11 @@ import Link from "next/link";
 const Landing = () => {
   return (
     <>
-      <main className="h-screen w-full bg-scroll bg-bg bg-no-repeat bg-cover max-h-screen flex"></main>
+      <main className="h-screen w-full bg-scroll bg-no-repeat bg-cover max-h-screen flex">
+      <div class="w-full">
+          <Image src="/landing.png" class="absolute top-0 left-0 overflow-x-hidden object-fit" alt="landing" width={1500} height={800} />
+      </div>
+      </main>
     </>
   );
 };
