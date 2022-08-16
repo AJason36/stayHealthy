ceaf2cf5f29272c25cd9293010bc18fa8268b3e8
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-16 23:21:24 +0700

    Update Landing.js

--- components/Landing.js
+++ components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
     <>
       <main className="h-screen w-full bg-scroll bg-no-repeat bg-cover max-h-screen flex">
-      <div class="w-full">
+      <div className="w-full">
           <Image src="/landing.png" className="absolute top-0 left-0 overflow-x-hidden object-fit" alt="landing" width={1500} height={800} />
       </div>
       </main>
